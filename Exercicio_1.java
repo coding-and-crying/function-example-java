5ca8b53ea9985a811e15a8825cbe184e0e7fb750
matheusgb <matheusgbr98@gmail.com>
2023-07-17 13:41:50 -0300

    java function example

---
+++ Exercicio_1.java
@@ -0,0 +1,9 @@
+package poo;
+
+public class Exercicio_1 {
+  public static double PI = 3.14159;
+
+  public static double circumference(double radius) {
+    return 2.0 * PI * radius;
+  }
+}
---
+++ Programa.java
@@ -0,0 +1,10 @@
+package poo;
+
+// import poo.Exercicio_1;
+
+public class Programa {
+  public static void main(String[] args) {
+    double contaFeita = poo.Exercicio_1.circumference(3.0);
+    System.out.println(contaFeita);
+  }
+}
